ff4276fa854bb1030cccdccc0910ccd0e0ad3d65
Aviu00 <aviu00@protonmail.com>
2024-02-06 09:48:33 +0300

    - add: New bleed alert

--- Content.Server/Body/Components/BloodstreamComponent.cs
+++ Content.Server/Body/Components/BloodstreamComponent.cs
@@ -166,14 +166,5 @@ namespace Content.Server.Body.Components
         /// </summary>
         [ViewVariables(VVAccess.ReadWrite)]
         public float StatusTime;
-
-        //WD-EDIT
-
-        /// <summary>
-        ///     Bool for bleeding alert.
-        /// </summary>
-        public bool IsBleeding => BleedAmount > 0;
-
-        //WD-EDIT
     }
 }

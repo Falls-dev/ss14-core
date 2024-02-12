65a15a8690b3c9fbedbb8074fabf31b22f980ed8
Aviu00 <aviu00@protonmail.com>
2024-02-12 09:33:00 +0300

    - fix: Mindshield fixes.

--- Content.Server/_White/Cult/Runes/Systems/CultSystem.Rune.cs
+++ Content.Server/_White/Cult/Runes/Systems/CultSystem.Rune.cs
@@ -32,6 +32,7 @@ using Content.Shared._White.Cult;
 using Content.Shared._White.Cult.Components;
 using Content.Shared._White.Cult.Runes;
 using Content.Shared._White.Cult.UI;
+using Content.Shared.Mindshield.Components;
 using Robust.Server.GameObjects;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Components;
@@ -61,7 +62,6 @@ public sealed partial class CultSystem : EntitySystem
     [Dependency] private readonly GunSystem _gunSystem = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly FlammableSystem _flammableSystem = default!;
-    [Dependency] private readonly SharedJobSystem _jobSystem = default!;
 
 
     public override void Initialize()
@@ -419,15 +419,9 @@ public sealed partial class CultSystem : EntitySystem
             // Проверка, является ли жертва целью
             _entityManager.TryGetComponent<MindContainerComponent>(target?.CurrentEntity, out var targetMind);
             var isTarget = mind!.Mind!.Value == targetMind?.Mind!.Value;
-            var jobAllowConvert = true;
-
-            if(_jobSystem.MindTryGetJob(mind.Mind!.Value, out var _, out var prototype))
-            {
-                jobAllowConvert = prototype.CanBeAntag;
-            }
 
             // Выполнение действия в зависимости от условий
-            if (canBeConverted && jobAllowConvert && !isTarget)
+            if (canBeConverted && !HasComp<MindShieldComponent>(victim.Value) && !isTarget)
             {
                 result = Convert(uid, victim.Value, args.User, args.Cultists);
             }

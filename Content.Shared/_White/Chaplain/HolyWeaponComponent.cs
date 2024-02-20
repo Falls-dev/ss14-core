295a54ea02e161f0b36ae4ca48bbdfe8d9d5d1af
Aviu00 <aviu00@protonmail.com>
2024-02-20 07:37:26 +0300

    - fix: Fix component granting.

--- Content.Shared/_White/Chaplain/HolyWeaponComponent.cs
+++ Content.Shared/_White/Chaplain/HolyWeaponComponent.cs
@@ -1,6 +1,8 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._White.Chaplain;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class HolyWeaponComponent : Component
 {
 }

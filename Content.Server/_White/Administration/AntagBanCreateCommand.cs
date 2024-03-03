0e7ec9d088b2b44a1a99d4eec13f0a6228092f4b
BuildTools <26607408+NairSark@users.noreply.github.com>
2024-03-04 00:02:42 +0500

    Init. files + systems

---
+++ Content.Server/_White/Administration/AntagBanCreateCommand.cs
@@ -0,0 +1,20 @@
+﻿using Robust.Shared.Console;
+
+namespace Content.Server._White.Administration;
+
+public sealed class AntagBanCreateCommand : IConsoleCommand
+{
+    public string Command => "antagban";
+    public string Description => "Bans player antag role.";
+    public string Help => "antagban <ckey> <role> <reason> <time> <isglobal>";
+
+    public async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        return;
+    }
+
+    public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return CompletionResult.FromHintOptions(CompletionHelper.SessionNames(), "CKEY");
+    }
+}
---
+++ Content.Server/_White/Administration/AntagRoleBanSystem.cs
@@ -0,0 +1,29 @@
+﻿using Content.Server.Administration.Managers;
+using Robust.Shared.Network;
+
+namespace Content.Server._White.Administration;
+// Antag Bans don't exist. (c) metalgearsloth - 01.02.2024
+public sealed class AntagRoleBanSystem : EntitySystem
+{
+    [Dependency] private readonly BanManager _roleBanManager = default!;
+    public override void Initialize()
+    {
+
+    }
+
+    public bool IsAntagBanned(NetUserId nuid)
+    {
+        return false;
+    }
+
+    public bool CreateAntagBan(NetUserId nuid) // need more args
+    {
+        return false;
+        //return _roleBanManager.CreateRoleBan(nuid);
+    }
+
+    public bool CreateAntagUnban(NetUserId nuid)
+    {
+        return false;
+    }
+}

e35d4becd276a0163a5759d1b18bd7dd74c35e9a
Mona Hmiza <you@example.com>
2024-03-08 19:38:53 +0300

    База, основа, фундамент, генштаб
    Strongest matches
    
    foundation
    infrastructure
    source
    Strong matches
    
    authority
    backbone
    basis
    core
    essence
    essential
    evidence
    fundamental
    groundwork
    heart
    key
    origin
    principal
    principle
    root
    underpinning
    Weak matches
    
    chief constituent
    important part
    primary element

---
+++ Content.Server/_Miracle/GameRules/Violence/ViolenceRuleComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server._Miracle.GameRules;
+
+[RegisterComponent]
+[Access(typeof(ViolenceRuleSystem))]
+public sealed partial class ViolenceRuleComponent : Component
+{
+}
---
+++ Content.Server/_Miracle/GameRules/Violence/ViolenceRuleSystem.cs
@@ -0,0 +1,8 @@
+﻿using Content.Server.GameTicking.Rules;
+
+namespace Content.Server._Miracle.GameRules;
+
+public sealed class ViolenceRuleSystem : GameRuleSystem<ViolenceRuleComponent>
+{
+
+}

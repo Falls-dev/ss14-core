89a314d87fa7e68bbe0e09b4ae780c108a774c1c
Remuchi <RemuchiOfficial@gmail.com>
2024-03-09 23:53:18 +0700

    add: система лежания и вставания

--- Content.Shared/Standing/StandingStateComponent.cs
+++ Content.Shared/Standing/StandingStateComponent.cs
@@ -1,18 +1,23 @@
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Standing
 {
-    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-    [Access(typeof(StandingStateSystem))]
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(StandingStateSystem))]
     public sealed partial class StandingStateComponent : Component
     {
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField]
+        [ViewVariables(VVAccess.ReadWrite), DataField]
         public SoundSpecifier DownSound { get; private set; } = new SoundCollectionSpecifier("BodyFall");
 
         [DataField, AutoNetworkedField]
-        public bool Standing { get; set; } = true;
+        public StandingState CurrentState { get; set; } = StandingState.Standing; // WD EDIT
+
+        /// <summary>
+        ///     Time required to get up.
+        /// </summary>
+        [DataField, AutoNetworkedField]
+        public TimeSpan StandingUpTime { get; set; } = TimeSpan.FromSeconds(1); // WD EDIT
 
         /// <summary>
         ///     List of fixtures that had their collision mask changed when the entity was downed.
@@ -21,4 +26,17 @@ namespace Content.Shared.Standing
         [DataField, AutoNetworkedField]
         public List<string> ChangedFixtures = new();
     }
+}
+
+[Serializable, NetSerializable]
+public sealed class ChangeStandingStateEvent : EntityEventArgs
+{
+}
+
+// WD EDIT
+public enum StandingState
+{
+    Lying,
+    GettingUp,
+    Standing
 }

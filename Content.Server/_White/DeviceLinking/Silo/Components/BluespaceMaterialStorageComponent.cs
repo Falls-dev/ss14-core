142b11213ae6feddb7af389f998a32c57c7985ab
drdth <drdtheuser@gmail.com>
2024-03-11 01:59:21 +0300

    add: prototypes for silo and silo ports

---
+++ Content.Server/_White/DeviceLinking/Silo/Components/BluespaceMaterialStorageComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.DeviceLinking;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server._White.DeviceLinking.Silo.Components;
+
+[RegisterComponent]
+public sealed partial class BluespaceMaterialStorageComponent : Component
+{
+    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<SinkPortPrototype>))]
+    public string Hub = "MaterialHub";
+
+    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<SinkPortPrototype>))]
+    public string LocalStorage = "MaterialLocalStorage";
+}
---
+++ Content.Server/_White/DeviceLinking/Silo/SiloSystem.cs
@@ -0,0 +1,18 @@
+﻿using Content.Server._White.DeviceLinking.Silo.Components;
+using Content.Server.DeviceLinking.Events;
+
+namespace Content.Server._White.DeviceLinking.Silo;
+
+public sealed class SiloSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<BluespaceMaterialStorageComponent, SignalReceivedEvent>(OnSignalReceived);
+    }
+
+    private void OnSignalReceived(EntityUid uid, BluespaceMaterialStorageComponent component, SignalReceivedEvent args)
+    {
+
+    }
+}

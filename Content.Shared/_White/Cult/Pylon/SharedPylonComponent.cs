f15b5909f75fc309c790080f368295bd09c1afda
Aviu00 <aviu00@protonmail.com>
2024-03-11 15:29:36 +0300

    - tweak: Better pylon placement & less pylon healing range.

--- Content.Shared/_White/Cult/Pylon/SharedPylonComponent.cs
+++ Content.Shared/_White/Cult/Pylon/SharedPylonComponent.cs
@@ -8,7 +8,7 @@ namespace Content.Shared._White.Cult.Pylon;
 public sealed partial class SharedPylonComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField("healingAuraRange")]
-    public float HealingAuraRange = 5f;
+    public float HealingAuraRange = 3f;
 
     [ViewVariables(VVAccess.ReadOnly), DataField("healingAuraDamage", required: true)]
     public DamageSpecifier HealingAuraDamage = default!;

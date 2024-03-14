15a54f5f3e4c574a19f050b0ce712700c1c0083e
drdth <drdtheuser@gmail.com>
2024-03-14 23:21:51 +0300

    add: new visuals to health analyzer and new graphic funny light

---
+++ Content.Shared/_White/Lighting/PointLightBatteryComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._White.Lighting;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class PointLightBatteryComponent : SharedPointLightComponent
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool RequireBattery = true;
+}
---
+++ Content.Shared/_White/Lighting/PointLightBatterySystem.cs
@@ -0,0 +1,25 @@
+﻿using Content.Shared.Lightning;
+using Content.Shared.PowerCell;
+using Content.Shared.PowerCell.Components;
+
+namespace Content.Shared._White.Lighting;
+
+public sealed class PointLightBatterySystem : SharedLightningSystem
+{
+    [Dependency] private readonly SharedPointLightSystem _pointLightSystem = default!;
+    [Dependency] private readonly SharedPowerCellSystem _cell = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<PointLightBatteryComponent, PowerCellChangedEvent>(OnBatteryLoose);
+    }
+
+    private void OnBatteryLoose(EntityUid uid, PointLightBatteryComponent component, PowerCellChangedEvent args)
+    {
+        if (!component.RequireBattery)
+            return;
+
+        var isBatteryCharged = _cell.HasDrawCharge(uid);
+        _pointLightSystem.SetEnabled(uid, isBatteryCharged && !args.Ejected, component);
+    }
+}

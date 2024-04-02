859a97f7af7508b0cc00470478b1713b48b22a44
Aviu00 <aviu00@protonmail.com>
2024-04-02 09:36:50 +0300

    - add: Update accents.

--- Content.Server/Speech/EntitySystems/MothAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/MothAccentSystem.cs
@@ -19,7 +19,13 @@ public sealed class MothAccentSystem : EntitySystem
         message = Regex.Replace(message, "z{1,3}", "zzz");
         // buZZZ
         message = Regex.Replace(message, "Z{1,3}", "ZZZ");
-        
+
+        // WD EDIT START
+        message = Regex.Replace(message, "з{1,3}", "ззз");
+
+        message = Regex.Replace(message, "З{1,3}", "ЗЗЗ");
+        // WD EDIT END
+
         args.Message = message;
     }
 }

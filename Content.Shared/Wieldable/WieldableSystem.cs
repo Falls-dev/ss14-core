fb9c6260ad133a99a8b34ee8b76c82ebc1a0e643
WarMechanic <69510347+WarMechanic@users.noreply.github.com>
2024-04-26 22:31:50 +1000

    fix wieldable guns not being able to cycle inhand (#27307)
    * Add UnwieldOverride variable that overrides unwielding inhand to enable other interactions
    
    * Give LMGs UnwieldOverride
    
    * logically inverted UnwieldOverride to UnwieldOnUse
    
    * fixed typo
    
    (cherry picked from commit 498109a833047d933a5f72edf87961542c516f58)

--- Content.Shared/Wieldable/WieldableSystem.cs
+++ Content.Shared/Wieldable/WieldableSystem.cs
@@ -136,7 +136,7 @@ public sealed class WieldableSystem : EntitySystem
 
         if (!component.Wielded)
             args.Handled = TryWield(uid, component, args.User);
-        else
+        else if (component.UnwieldOnUse)
             args.Handled = TryUnwield(uid, component, args.User);
     }
 

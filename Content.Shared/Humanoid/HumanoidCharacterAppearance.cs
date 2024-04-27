9e2df3002efd6f2a3666fe821acdfde79dc1e027
Errant <35878406+errant-4@users.noreply.github.com>
2024-04-27 09:04:51 +0300

    Vox sprite rework (#26985)
    weh
    
    (cherry picked from commit 257b04d27793b8c8853af57a9f53d361062d5d61)

--- Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
+++ Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
@@ -111,7 +111,8 @@ public sealed partial class HumanoidCharacterAppearance : ICharacterAppearance
             HumanoidSkinColor.HumanToned => Humanoid.SkinColor.HumanSkinTone(speciesPrototype.DefaultHumanSkinTone),
             HumanoidSkinColor.Hues => speciesPrototype.DefaultSkinTone,
             HumanoidSkinColor.TintedHues => Humanoid.SkinColor.TintedHues(speciesPrototype.DefaultSkinTone),
-            _ => Humanoid.SkinColor.ValidHumanSkinTone
+            HumanoidSkinColor.VoxFeathers => Humanoid.SkinColor.ClosestVoxColor(speciesPrototype.DefaultSkinTone),
+            _ => Humanoid.SkinColor.ValidHumanSkinTone,
         };
 
         return new(
@@ -172,6 +173,9 @@ public sealed partial class HumanoidCharacterAppearance : ICharacterAppearance
                 break;
             case HumanoidSkinColor.TintedHues:
                 newSkinColor = Humanoid.SkinColor.ValidTintedHuesSkinTone(newSkinColor);
+                break;
+            case HumanoidSkinColor.VoxFeathers:
+                newSkinColor = Humanoid.SkinColor.ProportionalVoxColor(newSkinColor);
                 break;
         }
 

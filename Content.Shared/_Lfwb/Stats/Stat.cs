e943e95d6c977729d6e1b8ae750b7636b4ea72a3
hailrakes <splatt.pr@gmail.com>
2024-04-29 11:36:13 +0500

    тз так сказать

--- Content.Shared/_Lfwb/Stats/Stat.cs
+++ Content.Shared/_Lfwb/Stats/Stat.cs
@@ -5,8 +5,8 @@ namespace Content.Shared._Lfwb.Stats;
 [Serializable, NetSerializable]
 public enum Stat : byte
 {
-    Strength = 0, // affects damage, health
-    Intelligence = 1, // affects something
-    Dexterity = 2, // affects move speed, attack speed, dodging/parry, hit chance
+    Strength = 0, // affects damage, health, doafters where need str
+    Intelligence = 1, // affects examine text, doafters where needs int, machinery
+    Dexterity = 2, // affects move speed, attack speed, dodging/parry, hit chance, doafters speedup
     Endurance = 3 // affects stamina
 }

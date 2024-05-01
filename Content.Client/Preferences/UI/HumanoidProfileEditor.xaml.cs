4ba1012128365b4f08207247b35916395f084008
Ciac32 <aknoxlor@gmail.com>
2024-04-30 23:28:13 -0400

    Fix Loadout Editor not closing when Character Editor is closed. (#27539)
    Remove duplicate UpdateRoleRequirements call screwing up update ordering
    
    (cherry picked from commit b160c429269df958aa9e9f1cde2b83677ba33e11)

--- Content.Client/Preferences/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Preferences/UI/HumanoidProfileEditor.xaml.cs
@@ -792,7 +792,6 @@ namespace Content.Client.Preferences.UI
             CharacterSlot = _preferencesManager.Preferences.SelectedCharacterIndex;
 
             UpdateAntagRequirements();
-            UpdateRoleRequirements();
             UpdateControls();
             ShowClothes.Pressed = true;
         }

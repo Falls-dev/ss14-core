7bba975192f9e35c67c1a02a8f09474b7284c8d3
hailrakes <splatt.pr@gmail.com>
2024-05-02 19:52:01 +0500

    nigga wtf

--- Content.Shared/_Lfwb/Stats/Stat.cs
+++ Content.Shared/_Lfwb/Stats/Stat.cs
@@ -5,7 +5,7 @@ namespace Content.Shared._Lfwb.Stats;
 [Serializable, NetSerializable]
 public enum Stat : byte
 {
-    Strength = 0, // health
+    Strength = 0,
     Intelligence = 1,
     Dexterity = 2, // affects move speed.
     Endurance = 3, // affects stamina

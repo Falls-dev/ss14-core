dafada399658f0a93bb00639c540fd1a1ac49979
grach <dropbox1319827@gmail.com>
2024-06-18 22:16:13 +0300

    showmanifest command

---
+++ Content.Client/GameTicking/Commands/ShowManifestCommand.cs
@@ -0,0 +1,28 @@
+﻿using Content.Client.GameTicking.Managers;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Client.GameTicking.Commands;
+
+[AnyCommand]
+public sealed class ShowManifestCommand : IConsoleCommand
+{
+    [Dependency] private readonly IEntitySystemManager _entitySystem = default!;
+    public string Command => "showmanifest";
+    public string Description => "Shows manifest if you closed one";
+    public string Help => "Usage: showmanifest";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var ticker = _entitySystem.GetEntitySystem<ClientGameTicker>();
+        var window = ticker._window;
+
+        if (window == null)
+        {
+            shell.WriteLine("This can only be executed while the game is not in a round.");
+            return;
+        }
+
+        window.OpenCentered();
+    }
+}

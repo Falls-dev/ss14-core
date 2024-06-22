7c0f7d1e749e96dbde8374ebeb9b92f6a23801e1
drdth <drdtheuser@gmail.com>
2024-06-22 13:39:51 +0300

    cleanup

--- Content.Shared/Doors/Systems/SharedDoorSystem.Bolts.cs
+++ Content.Shared/Doors/Systems/SharedDoorSystem.Bolts.cs
@@ -86,7 +86,6 @@ public abstract partial class SharedDoorSystem
         Dirty(ent, ent.Comp);
         UpdateBoltLightStatus(ent);
 
-
         var sound = value ? ent.Comp.BoltDownSound : ent.Comp.BoltUpSound;
         if (predicted)
             Audio.PlayPredicted(sound, ent, user: user);

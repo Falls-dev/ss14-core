a3845765105a4d7e6e130d0f8ede12794aa82dc8
drdth <drdtheuser@gmail.com>
2024-06-29 08:24:47 +0300

    fix: doorlights supports emagging now + fix emergency light

--- Content.Client/_White/Lighting/PointLight/Airlock/PointLightAirlockSystem.cs
+++ Content.Client/_White/Lighting/PointLight/Airlock/PointLightAirlockSystem.cs
@@ -44,10 +44,16 @@ public sealed class PointLightAirlockSystem : EntitySystem
         if (!args.AppearanceData.TryGetValue(DoorVisuals.State, out var state))
             return;
 
-        HandleState(uid, component, (DoorState) state);
+        if (!component.IsEmagged)
+            HandleState(uid, component, (DoorState) state);
 
         if (args.AppearanceData.TryGetValue(DoorVisuals.EmergencyLights, out var emergency))
-            ToggleLight(uid, component.YellowColor, component, (bool) emergency);
+        {
+            if ((bool) emergency)
+                ToggleLight(uid, component.YellowColor, component, (bool) emergency);
+            else if (!component.IsEmagged)
+                HandleState(uid, component, (DoorState) state);
+        }
 
         if (!args.AppearanceData.TryGetValue(DoorVisuals.BoltLights, out var boltsDown))
             return;
@@ -59,17 +65,25 @@ public sealed class PointLightAirlockSystem : EntitySystem
             else if (args.AppearanceData.TryGetValue(DoorVisuals.EmergencyLights, out var emergencyLights) && (bool) emergencyLights)
                 ToggleLight(uid, component.YellowColor, component);
             else
+            {
+                if (component.IsEmagged)
+                    component.IsEmagged = false;
                 HandleState(uid, component, (DoorState) state);
+            }
         }
 
         component.LastBoltsState = (bool) boltsDown;
-
     }
 
     private void HandleState(EntityUid uid, PointLightAirlockComponent component, DoorState state)
     {
         switch (state)
         {
+            case DoorState.Emagging:
+                ToggleLight(uid, component.RedColor, component);
+                component.IsEmagged = true;
+                break;
+
             case DoorState.Open:
                 ToggleLight(uid, component.BlueColor, component);
                 break;

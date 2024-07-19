fe08fd7b1429f7ca9cb5bd01a00acf13c3028e1a
drdth <drdtheuser@gmail.com>
2024-07-20 02:31:48 +0300

    flashbang rework

---
+++ Content.Shared/_White/BuffedFlashGrenade/FlashSoundSuppressionSystem.cs
@@ -0,0 +1,32 @@
+﻿using Content.Shared.Inventory.Events;
+using Content.Shared.Stunnable;
+
+namespace Content.Shared._White.BuffedFlashGrenade;
+
+public sealed class FlashSoundSuppressionSystem : EntitySystem
+{
+    [Dependency] private readonly SharedStunSystem _stunSystem = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<FlashSoundSuppressionComponent, GotEquippedEvent>(OnItemEquipped);
+        SubscribeLocalEvent<FlashSoundSuppressionComponent, GotUnequippedEvent>(OnItemUnequipped);
+    }
+
+    private void OnItemEquipped(EntityUid uid, FlashSoundSuppressionComponent component, GotEquippedEvent args)
+    {
+        EnsureComp<FlashSoundSuppressionComponent>(args.Equipee);
+    }
+
+    private void OnItemUnequipped(EntityUid uid, FlashSoundSuppressionComponent component, GotUnequippedEvent args)
+    {
+        RemComp<FlashSoundSuppressionComponent>(args.Equipee);
+    }
+
+    public void Stun(EntityUid target, float duration)
+    {
+        if (HasComp<FlashSoundSuppressionComponent>(target))
+            return;
+
+        _stunSystem.TryParalyze(target, TimeSpan.FromSeconds(duration / 1000f), true);
+    }
+}

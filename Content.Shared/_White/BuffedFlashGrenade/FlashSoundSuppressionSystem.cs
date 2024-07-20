f8771d0fa22a321f863c8405eda23158fdb62a6d
drdth <drdtheuser@gmail.com>
2024-07-20 04:48:28 +0300

    cleanup

--- Content.Shared/_White/BuffedFlashGrenade/FlashSoundSuppressionSystem.cs
+++ Content.Shared/_White/BuffedFlashGrenade/FlashSoundSuppressionSystem.cs
@@ -6,21 +6,6 @@ namespace Content.Shared._White.BuffedFlashGrenade;
 public sealed class FlashSoundSuppressionSystem : EntitySystem
 {
     [Dependency] private readonly SharedStunSystem _stunSystem = default!;
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<FlashSoundSuppressionComponent, GotEquippedEvent>(OnItemEquipped);
-        SubscribeLocalEvent<FlashSoundSuppressionComponent, GotUnequippedEvent>(OnItemUnequipped);
-    }
-
-    private void OnItemEquipped(EntityUid uid, FlashSoundSuppressionComponent component, GotEquippedEvent args)
-    {
-        EnsureComp<FlashSoundSuppressionComponent>(args.Equipee);
-    }
-
-    private void OnItemUnequipped(EntityUid uid, FlashSoundSuppressionComponent component, GotUnequippedEvent args)
-    {
-        RemComp<FlashSoundSuppressionComponent>(args.Equipee);
-    }
 
     public void Stun(EntityUid target, float duration)
     {

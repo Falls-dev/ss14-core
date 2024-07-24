c7cdb5a7768b023cb8b30303f9e9c7bf2aa2f0c2
drdth <drdtheuser@gmail.com>
2024-07-24 03:36:06 +0300

    Revert "Content changes for MapManager/System refactor (#26994)"
    This reverts commit ee96d8aa6636ef091ee823b49ff78ebc13bcce5e.

--- Content.Server/Salvage/SalvageSystem.Expeditions.cs
+++ Content.Server/Salvage/SalvageSystem.Expeditions.cs
@@ -166,7 +166,6 @@ public sealed partial class SalvageSystem
             _dungeon,
             _metaData,
             _transform,
-            _mapSystem,
             station,
             coordinatesDisk,
             missionParams,

4b83bd9d02c02e899fd31dd4cdf91ef1bfb1343b
Spatison <137375981+Spatison@users.noreply.github.com>
2024-07-25 02:42:40 +0300

    Fix: Shinano

---
+++ Content.Server/_White/Weapons/GunTriggerOnLandComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server._White.Weapons;
+
+[RegisterComponent]
+public sealed partial class GunTriggerOnLandComponent : Component
+{
+
+}
---
+++ Content.Server/_White/Weapons/GunTriggerOnLandSystem.cs
@@ -0,0 +1,20 @@
+﻿using Content.Server._White.Explosion;
+using Content.Shared.Weapons.Ranged.Events;
+
+namespace Content.Server._White.Weapons;
+
+public sealed class GunTriggerOnLandSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GunTriggerOnLandComponent, AmmoShotEvent>(OnAmmoShot);
+    }
+
+    private void OnAmmoShot(EntityUid uid, GunTriggerOnLandComponent component, ref AmmoShotEvent args)
+    {
+        foreach (var ammo in args.FiredProjectiles)
+        {
+            EnsureComp<TriggerOnLandComponent>(ammo);
+        }
+    }
+}

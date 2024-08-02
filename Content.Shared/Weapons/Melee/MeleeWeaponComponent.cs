d7e1c6cb9b6ab6126ab454d78f413fe32e30a490
Aviu00 <aviu00@protonmail.com>
2024-08-02 09:47:49 +0300

    - tweak: Reduce melee range.

--- Content.Shared/Weapons/Melee/MeleeWeaponComponent.cs
+++ Content.Shared/Weapons/Melee/MeleeWeaponComponent.cs
@@ -122,12 +122,11 @@ public sealed partial class MeleeWeaponComponent : Component
     [ViewVariables(VVAccess.ReadWrite), DataField]
     public FixedPoint2 ClickDamageModifier = FixedPoint2.New(1);
 
-    // TODO: Temporarily 1.5 until interactionoutline is adjusted to use melee, then probably drop to 1.2
     /// <summary>
     /// Nearest edge range to hit an entity.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite), DataField, AutoNetworkedField]
-    public float Range = 1.5f;
+    public float Range = 1.2f;
 
     /// <summary>
     /// Total width of the angle for wide attacks.

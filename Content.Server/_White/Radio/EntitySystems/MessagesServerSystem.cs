7651a23cc0955ea4a952ae95203a2a0ca1c10912
Spatison <137375981+Spatison@users.noreply.github.com>
2024-08-04 21:22:08 +0300

    add: Сообщения в ПДА

---
+++ Content.Server/_White/Radio/EntitySystems/MessagesServerSystem.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using Content.Server._White.CartridgeLoader.Cartridges;
+using Content.Server._White.Radio.Components;
+using Content.Server.DeviceNetwork.Systems;
+using Content.Shared._White.CartridgeLoader.Cartridges;
+using Content.Shared.CartridgeLoader;
+using Content.Shared.DeviceNetwork;
+
+
+namespace Content.Server._White.Radio.EntitySystems;
+
+public sealed class MessagesServerSystem : EntitySystem
+{
+    [Dependency] private readonly DeviceNetworkSystem _deviceNetworkSystem = default!;
+    [Dependency] private readonly SingletonDeviceNetServerSystem _singletonServerSystem = default!;
+    [Dependency] private readonly MessagesCartridgeSystem _messagesSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<MessagesServerComponent, DeviceNetworkPacketEvent>(OnPacketReceived);
+        SubscribeLocalEvent<MessagesServerComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, MessagesServerComponent component, ComponentInit args)
+    {
+        var query = EntityQueryEnumerator<MessagesCartridgeComponent>();
+
+        while (query.MoveNext(out var entityUid, out var cartridge))
+        {
+            if (!TryComp(entityUid, out CartridgeComponent? cartComponent))
+                continue;
+
+            _messagesSystem.TryGetUserName(cartComponent, out var name);
+
+            if (cartridge.UserUid == null)
+                continue;
+
+            component.NameDict[cartridge.UserUid.Value] = name;
+            cartridge.LastServer = uid;
+        }
+    }
+
+    /// <summary>
+    /// Reacts to packets received from clients
+    /// </summary>
+    private void OnPacketReceived(EntityUid uid, MessagesServerComponent component, DeviceNetworkPacketEvent args)
+    {
+        if (!_singletonServerSystem.IsActiveServer(uid))
+            return;
+        if (args.Data.TryGetValue<string>(MessagesNetworkKeys.NewName, out var name) && args.Data.TryGetValue<int>(MessagesNetworkKeys.UserId, out var userId))
+        {
+            component.NameDict[userId] = name;
+
+            var packet = new NetworkPayload();
+            _deviceNetworkSystem.QueuePacket(uid, args.SenderAddress, packet);
+        }
+        if (args.Data.TryGetValue<MessagesMessageData>(MessagesNetworkKeys.Message, out var message))
+            SendMessage(uid, component, message);
+    }
+
+    /// <summary>
+    /// Broadcasts a message into the network
+    /// </summary>
+    private void SendMessage(EntityUid uid, MessagesServerComponent component, MessagesMessageData message)
+    {
+        component.Messages.Add(message);
+
+        var packet = new NetworkPayload()
+        {
+            [MessagesNetworkKeys.Message] = message
+        };
+
+        _deviceNetworkSystem.QueuePacket(uid, null, packet);
+    }
+
+    /// <summary>
+    /// Returns the name of a given user
+    /// </summary>
+    public bool TryGetNameFromDict(EntityUid? uid, int key, out string name)
+    {
+        if (!TryComp(uid, out MessagesServerComponent? component))
+        {
+            name = Loc.GetString("messages-pda-connection-error");
+            return false;
+        }
+        if (component.NameDict.TryGetValue(key, out var keyValue))
+        {
+            name = keyValue;
+            return true;
+        }
+        name = Loc.GetString("messages-pda-user-missing");
+        return false;
+    }
+
+    /// <summary>
+    /// Returns the name dictionary cache
+    /// </summary>
+    public Dictionary<int, string> GetNameDict(EntityUid? uid)
+    {
+        if (!TryComp(uid, out MessagesServerComponent? component))
+            return new Dictionary<int, string>();
+        return component.NameDict;
+    }
+
+    /// <summary>
+    /// Returns list of messages between the two users
+    /// </summary>
+    public List<MessagesMessageData> GetMessages(EntityUid? uid, int id1, int id2)
+    {
+        if (!TryComp(uid, out MessagesServerComponent? component))
+            return [];
+        return
+        [
+            ..component.Messages.Where(message =>
+                message.SenderId == id1 && message.ReceiverId == id2 ||
+                message.SenderId == id2 && message.ReceiverId == id1)
+        ];
+    }
+
+}
+
+public static class MessagesNetworkKeys
+{
+    public const string NewName = "new_name";
+    public const string UserId = "user_id";
+    public const string Message = "message";
+}
---
+++ Content.Shared/_White/CartridgeLoader/Cartridges/MessagesUiState.cs
@@ -0,0 +1,39 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._White.CartridgeLoader.Cartridges;
+
+///<summary>
+/// The state of the messages app interface.
+/// Mode switches whether the UI should display a list of other users or a particular chat.
+/// Contents contains either the names of users and their ids in the messages system or simply a list of message strings.
+///</summary>
+[Serializable, NetSerializable]
+public sealed class MessagesUiState(MessagesUiStateMode mode, List<(string, int?)> contents, string? name = null) : BoundUserInterfaceState
+{
+    public List<(string, int?)>? Contents = contents;
+    public MessagesUiStateMode Mode = mode;
+    public string? Name = name;
+}
+
+///<summary>
+/// Enum representing the modes the program's UI can be in
+///</summary>
+[Serializable, NetSerializable]
+public enum MessagesUiStateMode : byte
+{
+    UserList,
+    Chat,
+    Error
+}
+
+///<summary>
+/// Data of a single message in the system, containing the ids of the sender and recipient, the text content and the time it was sent.
+///</summary>
+[Serializable, NetSerializable]
+public struct MessagesMessageData
+{
+    public int SenderId;
+    public int ReceiverId;
+    public string Content;
+    public TimeSpan Time;
+}

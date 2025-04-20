1b5e6725acfef7d2c095d404ed01392bfec6ba95
NR <66198468+withoutcode333@users.noreply.github.com>
2025-04-20 19:29:12 +0500

    baza (govno)

---
+++ Content.Client/_White/Contract/UI/ContractorBUI.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Client._White.Contract.UI;
+
+public sealed class ContractorBUI : BoundUserInterface
+{
+    private ContractorWindow? _window;
+
+    public ContractorBUI(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+}
---
+++ Content.Shared/_White/Contract/ContractorBoundUserInterfaceState.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._White.Contract
+{
+    [Serializable, NetSerializable]
+    public enum ContractorUplinkUiKey
+    {
+        Key
+    }
+
+    public sealed class ContractorBoundUserInterfaceState : BoundUserInterfaceState
+    {
+        public readonly List<ContractEntry> Contracts;
+        public readonly int Reputation;
+
+        public ContractorBoundUserInterfaceState(List<ContractEntry> contracts, int reputation)
+        {
+            Contracts = contracts;
+            Reputation = reputation;
+        }
+    }
+}
